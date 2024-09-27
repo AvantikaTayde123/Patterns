297d8054edf0a672aba50f1a0b65a5e611b9b31e
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-09-27 15:25:59 +0530

    Create HollowSquare.java

---
+++ HollowSquare.java
@@ -0,0 +1,19 @@
+
+public class Example1 {
+    public static void main(String[] args) {
+        int i, j;
+
+        for(i=0;i<6;i++){
+            for(j=0;j<6;j++){
+                if(i==0 || j==0|| i==5|| j==5){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+            System.out.println();
+        }
+    }
+    
+}
