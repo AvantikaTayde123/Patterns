76648ed87c4d39b51558ba765e6bc749a31c84c7
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-09-27 15:27:02 +0530

    Create Traingle.java

---
+++ Traingle.java
@@ -0,0 +1,35 @@
+/*public class Example2 {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=0;i<6;i++){
+            for(j=0;j<6-i;j++){
+                System.out.print(" ");
+            }
+            for(j=0;j<=i;j++){
+                System.out.print(i+" ");
+            }
+        
+           System.out.println();
+        }
+    }
+}*/
+
+
+
+package Patterns;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=0;i<6;i++){
+            for(j=0;j<6;j++){
+                System.out.print(" ");
+            }
+            for(j=0;j<=i;j++){
+                System.out.print(i+" ");
+            }
+        
+           System.out.println();
+        }
+    }
+}
