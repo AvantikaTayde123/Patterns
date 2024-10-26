432526d6a0319e5a07134fd2a38b5e85998972c9
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-10-26 08:27:42 +0530

    Add files via upload

---
+++ Example.js
@@ -0,0 +1,22 @@
+function hlw(){
+    console.log("hello");
+}
+
+function add(a,b){
+     let p=parseInt(prompt("Enter a no"))
+    let q=parseInt(prompt("Enter second no"))
+    console.log(p+q);
+    
+}
+
+function mul(){
+    for(let i=0;i<=10;i++){
+        console.log(i+", ")
+    }
+}
+
+//add(a,b);
+add(3,4);
+add('hello','world')
+hlw();
+mul();
