3fe9e73a2e74afcec63b8680b66576365638938c
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-12-17 15:45:27 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,14 @@
+class p1{
+    public static void main(String[] args) {
+        int i,j;
+        for(i=0;i<=2;i++){
+            for(j=0;j<=i;j++){
+         System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
