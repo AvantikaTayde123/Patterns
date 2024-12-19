c2cd31bc8218f90350e39e95271048b732df9b0a
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-12-19 11:36:22 +0530

    Create Pattern1.java

---
+++ Pattern1.java
@@ -0,0 +1,24 @@
+class Main {
+    public static void main(String[] args) {
+        int i, j;
+        for(i=1;i<=10;i++){
+            for(j=1;j<=i;j++){
+        
+        System.out.print(j);
+    }
+    System.out.println();
+}
+}
+}
+
+o/p
+1
+12
+123
+1234
+12345
+123456
+1234567
+12345678
+123456789
+12345678910
