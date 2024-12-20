560bbf7dd1f038cae42ef58b6f3c4fe0ce5564f6
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-12-20 11:46:43 +0530

    Create P4.java

---
+++ P4.java
@@ -0,0 +1,22 @@
+class P4{
+    public static void main(String[] args) {
+        int i,j,n=5;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i;j++){
+                System.out.print(" ");
+
+            }
+                for(j=1;j<=i;j++){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+}
+}
+
+
+    1
+   12
+  123
+ 1234
+12345
