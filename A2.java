c29458b602dd2ff86968ff61d65090c47d6f4df7
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-12-23 23:49:56 +0530

    Add files via upload

---
+++ A2.java
@@ -0,0 +1,14 @@
+class A2 {
+    public static void main(String[] args) {
+        int num =12397;
+        num=num%10000;
+        num =num/10;
+        System.out.println(num);
+    }
+    
+}
+//o/p 239
+//leader element (maxmimum occurance no is leader element).
+
+
+
