1fe8ec29af2a3e3800a6b90ecb505061f2e0e495
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-12-23 23:50:17 +0530

    Add files via upload

---
+++ A3.java
@@ -0,0 +1,20 @@
+class A3{
+    public static void main(String[] args) {
+        String str1 = "HELLO";
+        String str2 = "WORLD";
+        
+        for (int i = 0; i < str1.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" "); 
+            }
+            System.out.println(str1.charAt(i)); 
+        }
+        
+        for (int i = 0; i < str2.length(); i++) {
+            for (int j = 0; j < i; j++) {
+               System.out.print(" "); 
+            }
+            System.out.println(str2.charAt(i));
+        }
+    }
+}
