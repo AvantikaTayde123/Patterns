278a023a43d4264f91dd28b246bd234ea5cbf99b
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2024-12-24 23:50:21 +0530

    Add files via upload

---
+++ A5.java
@@ -0,0 +1,23 @@
+class A5 {
+    public static void main(String[] args) {
+        int n=7;
+        char ch ='A';
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<i;j++){
+                System.out.print(j);
+            }
+            for(int j=i;j<=n;j++){
+                System.out.print(ch);
+            }
+            ch++;
+            System.out.println();
+        }
+    } 
+}
+// AAAAAAA
+// 1BBBBBB
+// 12CCCCC
+// 123DDDD
+// 1234EEE
+// 12345FF
+// 123456G
