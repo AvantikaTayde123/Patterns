06ee1305fbdd077d1246c733acc5ae2ec601d41d
Avantika Tayde <103492326+AvantikaTayde123@users.noreply.github.com>
2025-01-14 23:59:35 +0530

    Create PatternExample11.java

---
+++ PatternExample11.java
@@ -0,0 +1,23 @@
+public class NumberPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k);
+            }
+            for (int k = i - 1; k >= 1; k--) {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+}
+//o/p
+    1
+   121
+  12321
+ 1234321
+123454321
